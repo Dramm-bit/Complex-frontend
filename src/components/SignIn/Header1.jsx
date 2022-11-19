df131509f148edc4afeab9f3200f1a5e4328257d
Dramm-bit <77645019+Dramm-bit@users.noreply.github.com>
2022-11-19 15:22:20 -0500

    added the first component design and reorganized the src

---
+++ src/components/SignIn/Header1.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import '../../styles/components/Header1.css'
+import logo from '../../resources/logo.jpg';
+
+export default function Header1() {
+    return (
+
+        <section className="header">
+            <header className="header__header1">
+                <div className="header__group-logo">
+                     <img src={logo} className="header__img logo--standard"></img>
+                    <div className="header__title company--standard">ConSM</div>
+                </div>
+                <div className="header__botton-header1">
+                    <button className="header__button button--style2" >Log in</button>
+                    <button className="header__button button--standard">Register</button>
+                </div>
+            </header>
+        </section>
+
+    );
+}
